d5540cf98c3e46d9f7dfb598cf0848963f3d53c1
laraholand <laraholand0921@gmail.com>
2025-12-30 07:05:05 +0600

    YML fix

---
+++ lib/HomePage.dart
@@ -0,0 +1,9 @@
+class Homepage extends StatelessWidget {
+  const Homepage({super.key});  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -4,40 +4,26 @@ void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
       theme: ThemeData(
-        // useMaterial3: false,
         primarySwatch: Colors.blue,
       ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: AuthPags(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
+class AuthPags extends StatelessWidget {
+  const AuthPags({super.key});  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
+        title: const Text("Item RDC"),
       ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
-        ),
-      ),
+      
     );
   }
 }
